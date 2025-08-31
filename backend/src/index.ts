454dda5cd847991ce3ecc37db0ac9af35b7ec701
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-09-01 01:51:51 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created main application component (App.tsx)
    - Set up global CSS with Tailwind
    - Configured entry point (main.tsx) for React application
    - Added TypeScript environment definitions (vite-env.d.ts)
    - Configured TypeScript for application and node environments
    - Created Vite configuration file for React and Tailwind integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,11 @@ import 'dotenv/config'
 import express, { Request, Response } from 'express';
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express + TypeScript Server!');
