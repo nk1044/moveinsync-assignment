c5c327471650730cdb175fa3dfe5356c67e07d69
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-09-01 01:38:19 +0530

    feat: initialize backend with TypeScript, Express, and Prisma setup
    - Added package.json for backend dependencies and scripts
    - Created Prisma schema file for database configuration
    - Implemented basic Express server in index.ts
    - Configured TypeScript settings in tsconfig.json

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express + TypeScript Server!');
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server is running at PORT:${port}`);
+});
